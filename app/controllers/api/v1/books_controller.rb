c2e047ad1d0af606e2cc80adb3a84811a2938053
nomeaningcode <williamsalvaradovs1520@hotmail.com>
2024-08-16 17:47:31 -0600

    paginacion index, y rpec por controlador

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,9 +1,11 @@
 module Api
   module V1
     class BooksController < ApplicationController
+      MAX_PAGINATION_LIMIT = 100
+      
       def index
-        # books = Book.limit(params(:limit)).offset(params[:offset])
-        books = Book.all
+        books = Book.limit(limit).offset(params[:offset])
+        # books = Book.all
         render json: BooksRepresenter.new(books).as_json
       end
 
@@ -25,6 +27,12 @@ module Api
       end
 
       private
+      def limit
+        [params.fetch(:limit, MAX_PAGINATION_LIMIT).to_i, MAX_PAGINATION_LIMIT].min
+      end
+      def author_params
+        params.require(:author).permit(:first_name, :last_name, :age)
+      end
       def book_params
         params.require(:book).permit(:title, :author_id)
       end
